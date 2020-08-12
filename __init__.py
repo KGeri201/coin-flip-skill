a9750655bee9202683fec708ccf764d00606eedd
Gergö Kranz <kranz.geri@gmail.com>
2020-08-12 21:40:59 +0100

    First Version

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+import random
 
 class CoinFlip(MycroftSkill):
     def __init__(self):
@@ -7,12 +7,15 @@ class CoinFlip(MycroftSkill):
 
     @intent_file_handler('flip.coin.intent')
     def handle_flip_coin(self, message):
-        side = ''
-
-        self.speak_dialog('flip.coin', data={
-            'side': side
-        })
-
+        try:
+            coin = ["head", "number"]
+            side = random.choice(coin)
+            #result = {'side': message.data.get("side")}
+            self.speak_dialog('flip.coin', data={'side': side})
+	    #self.speak_dialog("response", data=result)
+            pass
+        except:
+            #self.speak_dialog("error")
 
 def create_skill():
     return CoinFlip()
